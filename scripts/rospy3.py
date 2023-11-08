b6881217c5976f9231ed4791863df09975d1ae32
ChienDavid <DVChienbkvn@gmail.com>
2023-11-08 18:28:01 +0900

    updated rospy3

--- scripts/rospy3.py
+++ scripts/rospy3.py
@@ -5,7 +5,7 @@ import rospy
 class TEST:
     def __init__(self):
         print("Let's test")
-        print("First: Test Python3 working in ROS")
+        print("Test Python3 working in ROS")
 
 
 def main():
